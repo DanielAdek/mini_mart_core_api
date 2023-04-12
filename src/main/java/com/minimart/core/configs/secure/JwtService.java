19a7e8b21541cf893914a536dbff7119bb62135f
DanielAdek <maildaniel.me1@gmail.com>
2023-04-12 19:10:30 +0100

    security setup for extracting all claims

--- src/main/java/com/minimart/core/configs/secure/JwtService.java
+++ src/main/java/com/minimart/core/configs/secure/JwtService.java
@@ -1,14 +1,34 @@
 package com.minimart.core.configs.secure;
 
+import java.security.Key;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 
 @Service
 public class JwtService {
-  
-  private Claims extractAllClaims(String claims) {
 
+  @Value("${application.security.jwt_secret.key}")
+  private String SECRET_KEY;
+  
+  private Claims extractAllClaims(String token) {
+    return Jwts
+            .parserBuilder()
+            .setSigningKey(getSignKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+  }
+
+
+  private Key getSignKey() {
+    byte[] keybBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keybBytes);
   }
 }
